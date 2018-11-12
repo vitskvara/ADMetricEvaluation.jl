90ebb10710688d08118819df99d4bc9c79dd352e
vit.skvara <vit.skvara@gmail.com>
2018-11-12 17:26:34 +0100

    correlation grid plots

--- experiments/process.jl
+++ experiments/process.jl
@@ -1,12 +1,7 @@
-#using Plots
 using CSV
 using DataFrames
-#using StatPlots
-
-dataset = ARGS[1]
-#dataset = "iris"
-isubd = (length(ARGS) > 1) ? isubd = Int(Meta.parse(ARGS[2])) : 0
-data_path = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_data"
+using PyPlot
+using Statistics
 
 loaddata(dataset::String, path = data_path) = 
 	map(x->CSV.read(joinpath(path, dataset, x)), readdir(joinpath(path, dataset)))
@@ -17,50 +12,98 @@ mergesubd(name::String, df_list::Array{DataFrame,1}, metrics::Array{Symbol, 1}) 
 	mergeds(filter(x->x[:dataset][1]==name,df_list), metrics)
 mergesubd(names::Array{String,1}, df_list::Array{DataFrame,1}, metrics::Array{Symbol, 1}) = 
 	mergeds(filter(x->x[:dataset][1] in names,df_list), metrics)
-
-dfs = loaddata(dataset)
-subd_list = subdatasets(dfs)
-subd = subd_list[isubd]
-metrics = [:auc, :auc_weighted, :auc_at_5, :prec_at_5, :tpr_at_5, :vol_at_5]
-models = ["kNN", "IF", "LOF", "OCSVM"]
-
-merged_df = mergesubd(subd, dfs, metrics)
-for metric in metrics
-	merged_df[metric] = Float64.(merged_df[metric])
+function linear_fit(x,y)
+	# y = Xb, where X = [ones', x']'
+	s = size(x)
+	n = s[1]
+	X = hcat(ones(n), x)
+	Xt = Array(transpose(X))
+	b = inv(Xt*X)*Xt*y
+	return b
 end
+function plot_linear_fit(x,y)
+	b = linear_fit(x,y)
+	_x = [minimum(x), maximum(x)]
+	_y = hcat(ones(2), _x)*b
+	plot(_x, _y, c="k", lw=1)
+end
+function correlation_grid_plot(dataset, isubd, data_path;
+		metrics = [:auc, :auc_weighted, :auc_at_5, :prec_at_5, :tpr_at_5, :vol_at_5],
+		models = ["kNN", "IF", "LOF", "OCSVM"])
+	dfs = loaddata(dataset)
+	subd_list = subdatasets(dfs)
+	isubd = min(isubd, length(subd_list))
+	subd = subd_list[isubd]
 
-using PyPlot
-Nm = length(metrics)
-figure()
-global n = 0
-for (j, m2) in enumerate(metrics)
-	for (i, m1) in enumerate(metrics)
-		n += 1
-		subplot(Nm, Nm, n)
-		if j == Nm xlabel(String(m1)) end
-		if i == 1 ylabel(String(m2)) end
+	merged_df = mergesubd(subd, dfs, metrics)
+	for metric in metrics
+		merged_df[metric] = Float64.(merged_df[metric])
+	end
 
-		if i == j
+	Nm = length(metrics)
+	figure()
+	global n = 0
+	for (j, m2) in enumerate(metrics)
+		for (i, m1) in enumerate(metrics)
+			n += 1
+			subplot(Nm, Nm, n)
+			if j == Nm xlabel(String(m1)) end
+			if i == 1 ylabel(String(m2)) end
+
+			# first, create a dummy scatter plot to get the x axis limits
 			for model in models
 				mdf = filter(x->x[:model]==model, merged_df)
-				PyPlot.plt[:hist](mdf[m1], 20, alpha = 1.0, label = model, density = true,
-					histtype = "step")
+				PyPlot.scatter(mdf[m1], mdf[m2], c="w")
 			end
-			if i==j==Nm legend() end
-		elseif j>i
-			for model in models
-				mdf = filter(x->x[:model]==model, merged_df)
-				PyPlot.scatter(mdf[m1], mdf[m2], s = 15, alpha = 0.2)
+			ax = plt[:gca]()
+			_xlim = ax[:get_ylim]()
+			#ax[:cla]()
+
+			if i == j
+				for model in models
+					mdf = filter(x->x[:model]==model, merged_df)
+					PyPlot.plt[:hist](mdf[m1], 20, alpha = 1.0, label = model, density = true,
+						histtype = "step")
+				end
+				xlim(_xlim)
+				if i==j==1 legend() end
+			elseif j>i
+				_x = []
+				_y = []
+				for model in models
+					mdf = filter(x->x[:model]==model, merged_df)
+					PyPlot.scatter(mdf[m1], mdf[m2], s = 15, alpha = 0.2)
+					push!(_x, mdf[m1])
+					push!(_y, mdf[m2])
+				end
+				# add the fit line
+				plot_linear_fit(vcat(_x...), vcat(_y...))
+				# add the correlation coefficient
+				r = round(Statistics.cor(vcat(_x...), vcat(_y...)),digits=2)
+				#text(0.1,0.9,"R=$r", size=8)
+				_line = plt[:Line2D]([1], [1],color="w")
+				legend([_line],["R=$r"], frameon=false)
+
+			else
+				for model in models
+					mdf = filter(x->x[:model]==model, merged_df)
+					PyPlot.scatter(mdf[m1], mdf[m2], s = 15, alpha = 0.2)
+				end
+				#mdf = filter(x->x[:model] in models, merged_df)
+				#PyPlot.plt[:hist2d](mdf[m1], mdf[m2], 20, cmap = "hot_r")
 			end
-		else
-			for model in models
-				mdf = filter(x->x[:model]==model, merged_df)
-				PyPlot.scatter(mdf[m1], mdf[m2], s = 15, alpha = 0.2)
-			end
-			#mdf = filter(x->x[:model] in models, merged_df)
-			#PyPlot.plt[:hist2d](mdf[m1], mdf[m2], 20, cmap = "hot_r")
 		end
 	end
+	suptitle(subd)
+	show()
 end
-suptitle(subd)
-show()
+
+### MAIN ###
+if basename(PROGRAM_FILE) == basename(@__FILE__)
+
+	dataset = ARGS[1]
+	#dataset = "iris"
+	isubd = (length(ARGS) > 1) ? isubd = Int(Meta.parse(ARGS[2])) : 1
+	data_path = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_data"
+	correlation_grid_plot(dataset, isubd, data_path)
+end
