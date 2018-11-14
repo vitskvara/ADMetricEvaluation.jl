2b9cf46a18829597ca03d8d83cbbcc9e7c15278e
vit.skvara <vit.skvara@gmail.com>
2018-11-14 11:33:35 +0100

    plot datasets no if

--- experiments/plot_datasets_no_if.jl
+++ experiments/plot_datasets_no_if.jl
@@ -1,15 +1,27 @@
-include("plot_dataset.jl")
-data_path = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_data"
+using ADMetricEvaluation
+using PyPlot
+
+if host == "vit"
+	data_path = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_data"
+	dataset_info = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/dataset_overview.csv"
+	outpath = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_dataset_plots_no_if"
+elseif host == "axolotl.utia.cas.cz"
+	data_path = "/home/skvara/work/anomaly_detection/data/metric_evaluation/umap_data"
+	outpath = "/home/skvara/work/anomaly_detection/data/metric_evaluation/umap_dataset_plots_no_if"
+	dataset_info = "/home/skvara/work/anomaly_detection/data/metric_evaluation/dataset_overview.csv"
+end
+
 datasets = readdir(data_path)
-outpath = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_dataset_plots_no_if"
 mkpath(outpath)
 models = ["kNN", "LOF", "OCSVM"]
+
 for dataset in datasets
 	_subdatasets = unique(map(x->vcat(split(x,"_")[1:end-1]...)[1], 
 		readdir(joinpath(data_path,dataset))))
 	for (n, subdataset) in enumerate(_subdatasets)
 		try
-			f = correlation_grid_plot(dataset, n, data_path, models = models)
+			f = ADMetricEvaluation.correlation_grid_plot(dataset, n, data_path;
+				dataset_info = dataset_info, models=models)
 			savefig(joinpath(outpath, "$subdataset.png"))
 			close()
 			println("$subdataset saved!")
