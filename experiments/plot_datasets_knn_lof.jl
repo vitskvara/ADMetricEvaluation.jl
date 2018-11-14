3341cee9597b296eddf31c99aacbac2d6eee571f
vit.skvara <vit.skvara@gmail.com>
2018-11-14 11:37:47 +0100

    knn lof

---
+++ experiments/plot_datasets_knn_lof.jl
@@ -0,0 +1,32 @@
+using ADMetricEvaluation
+using PyPlot
+
+host = gethostname()
+if host == "vit"
+	data_path = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_data"
+	dataset_info = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/dataset_overview.csv"
+	outpath = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_dataset_plots_knn_lof"
+elseif host == "axolotl.utia.cas.cz"
+	data_path = "/home/skvara/work/anomaly_detection/data/metric_evaluation/umap_data"
+	outpath = "/home/skvara/work/anomaly_detection/data/metric_evaluation/umap_dataset_plots_knn_lof"
+	dataset_info = "/home/skvara/work/anomaly_detection/data/metric_evaluation/dataset_overview.csv"
+end
+
+datasets = readdir(data_path)
+mkpath(outpath)
+models = ["kNN", "LOF"]
+for dataset in datasets
+	_subdatasets = unique(map(x->vcat(split(x,"_")[1:end-1]...)[1], 
+		readdir(joinpath(data_path,dataset))))
+	for (n, subdataset) in enumerate(_subdatasets)
+		try
+			f = ADMetricEvaluation.correlation_grid_plot(dataset, n, data_path;
+				dataset_info = dataset_info, models=models)
+			savefig(joinpath(outpath, "$subdataset.png"))
+			close()
+			println("$subdataset saved!")
+		catch
+			println("$subdataset not saved!")
+		end
+	end
+end
