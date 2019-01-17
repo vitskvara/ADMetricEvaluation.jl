5d0d0e370fe8fc0bdc8188d551c5dcabacba99b0
vit.skvara <vit.skvara@gmail.com>
2019-01-17 16:39:54 +0100

    knn umap parallel

--- experiments/run_umap_knn_parallel.jl
+++ experiments/run_umap_knn_parallel.jl
@@ -22,11 +22,11 @@ using UCI
 	host = gethostname()
 	#master path where data will be stored
 	if host == "vit"
-		outpath = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/data_contaminated"
+		outpath = "/home/vit/vyzkum/anomaly_detection/data/metric_evaluation/umap_data_contaminated"
 	elseif host == "axolotl.utia.cas.cz"
-		outpath = "/home/skvara/work/anomaly_detection/data/metric_evaluation/full_data_contaminated"
+		outpath = "/home/skvara/work/anomaly_detection/data/metric_evaluation/umap_data_contaminated"
 	elseif host == "soroban-node-03"
-		outpath = "/compass/home/skvara/anomaly_detection/data/metric_evaluation/full_data_contaminated"
+		outpath = "/compass/home/skvara/anomaly_detection/data/metric_evaluation/umap_data_contaminated"
 	end
 
 	mkpath(outpath)
