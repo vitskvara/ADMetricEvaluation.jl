6338cfd6e94189329d0744572b9fb0b9dda3927e
Vít Škvára <vit.skvara@gmail.com>
2019-08-27 18:29:36 +0200

    changed the experimental setting to all use 1000 MC iterations

--- experiments/scripts_f1/run_experiment_full.sh
+++ experiments/scripts_f1/run_experiment_full.sh
@@ -2,15 +2,8 @@
 dataset=$1
 # first do the easier
 trcont=0.00
-julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --mc-volume-iters=1 --models kNN LOF OCSVM
+julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF
 trcont=0.01
-julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --mc-volume-iters=1 --models kNN LOF OCSVM
+julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF 
 trcont=0.05
-julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --mc-volume-iters=1 --models kNN LOF OCSVM
-# then do IF that need MC volume computation and takes a long time
-trcont=0.00
-julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --models IF
-trcont=0.01
-julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --models IF
-trcont=0.05
-julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --models IF
+julia ../run_experiment_f1.jl $dataset full_f1_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF
--- experiments/scripts_f1/run_experiment_umap.sh
+++ experiments/scripts_f1/run_experiment_umap.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 dataset=$1
 trcont=0.00
-julia ../run_experiment_f1.jl $dataset umap_f1_contaminated-$trcont $trcont --umap --mc-volume-iters=10 --models kNN LOF OCSVM
-julia ../run_experiment_f1.jl $dataset umap_f1_contaminated-$trcont $trcont --umap --models IF
+julia ../run_experiment_f1.jl $dataset umap_f1_contaminated-$trcont $trcont --umap --mc-volume-iters=1000 --models kNN LOF OCSVM IF
