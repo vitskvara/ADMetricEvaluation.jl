85f799348cb678e6f1a9aeb4ea75d0cb32130956
Vít Škvára <vit.skvara@gmail.com>
2020-02-10 09:03:33 +0100

    full bootstrapping

--- experiments/bootstrapping/run_experiment_full.sh
+++ experiments/bootstrapping/run_experiment_full.sh
@@ -2,8 +2,8 @@
 dataset=$1
 # first do the easier
 trcont=0.00
-julia ../run_experiment_f1.jl $dataset full_bootstrapping_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF --robust-measures
+/compass/home/skvara/julia-1.3.1/bin/julia ../run_experiment_f1.jl $dataset full_bootstrapping_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF --robust-measures
 trcont=0.01
-julia ../run_experiment_f1.jl $dataset full_bootstrapping_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF --robust-measures
+/compass/home/skvara/julia-1.3.1/bin/julia ../run_experiment_f1.jl $dataset full_bootstrapping_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF --robust-measures
 trcont=0.05
-julia ../run_experiment_f1.jl $dataset full_bootstrapping_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF --robust-measures
+/compass/home/skvara/julia-1.3.1/bin/julia ../run_experiment_f1.jl $dataset full_bootstrapping_contaminated-$trcont $trcont --mc-volume-iters=1000 --models kNN LOF OCSVM IF --robust-measures
--- experiments/bootstrapping/run_experiment_umap.sh
+++ experiments/bootstrapping/run_experiment_umap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dataset=$1
 trcont=0.00
-julia ../run_experiment_f1.jl $dataset umap_bootstrapping_contaminated-$trcont $trcont --umap --robust-measures --mc-volume-iters=1000 --models kNN OCSVM LOF IF
+/compass/home/skvara/julia-1.3.1/bin/julia ../run_experiment_f1.jl $dataset umap_bootstrapping_contaminated-$trcont $trcont --umap --robust-measures --mc-volume-iters=1000 --models kNN OCSVM LOF IF
